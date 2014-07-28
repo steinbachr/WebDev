bed27ca957e395685fbc4804f14f69ef02b61fd8
Bobby <steinbach.rj@gmail.com>
2014-07-28 01:06:48 -0400

    some changes to text / style

--- public/scripts/script.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    $('.container').height($(document).height());
-    $('.imgur-container').imgagine({
-        boxViewSel: '.generate-boxes',
-        gridViewSel: '.generate-grid',
-        regenerateSel: '.generate-random'
-    });
-})
