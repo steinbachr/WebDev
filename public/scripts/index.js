7209b566ad65a9b58936a5a9d03da4e4a7e5f2aa
Bobby <steinbach.rj@gmail.com>
2015-05-31 17:04:07 -0400

    brought rukkus screenshots + info up to date

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -55,7 +55,9 @@ $(document).ready(function() {
         {src: "public/images/product/rukkus/performer.png", alt: "performer activity", caption: "check out all of a performer's nearby (and far away) events"},
         {src: "public/images/product/rukkus/tickets1.png", alt: "ticket map 1", caption: "use the fully interactive map to find the perfect tickets"},
         {src: "public/images/product/rukkus/tickets2.png", alt: "ticket map 2", caption: "apply filters, zoom, and view real seat views to narrow your search"},
-        {src: "public/images/product/rukkus/tickets3.png", alt: "ticket map 3", caption: "beautiful!"}
+        {src: "public/images/product/rukkus/tickets3.png", alt: "ticket map 3", caption: "beautiful!"},
+        {src: "public/images/product/rukkus/order_details.png", alt: "order details", caption: "easily track and view past orders"},
+        {src: "public/images/product/rukkus/scan_tickets.png", alt: "scan tickets", caption: "No more printing out paper tickets, scan your tickets directly at the door!<br /><br /> *for select venues only"}
     ];
 
 
